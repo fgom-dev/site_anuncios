25f0be34fbc1c83e9e5743e413c0bb547e6b0338
fgom-dev <fnd.gomes02@gmail.com>
2020-03-15 22:24:55 -0300

    Adicionado pagina de anuncio

--- anuncios/urls.py
+++ anuncios/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home, categoria
+from .views import home, categoria, anuncio
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('categoria/<int:categoria_id>', categoria, name='categoria'),
+    path('anuncio/<int:anuncio_id>', anuncio, name='anuncio'),
 ]
